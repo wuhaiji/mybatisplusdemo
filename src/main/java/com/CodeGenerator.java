1f7411e5059ae29cf5abc4803cea04e6545f0ad0
wuhj <1475351569@qq.com>
2020-09-15 11:04:33 +0800

    55

--- src/main/java/com/CodeGenerator.java
+++ src/main/java/com/CodeGenerator.java
@@ -1,4 +1,4 @@
-package com.whj.mybatisplusdemo;
+package com;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
@@ -8,8 +8,6 @@ import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,22 +17,20 @@ public class CodeGenerator {
     //项目路径
     private static String canonicalPath = "";
     //基本包名
-    private static String basePackage = "com.blb.typlatform";
-    //    private static String moduleName = "mybatisplusdemo";
+    private static String basePackage = "com.whj.demo";
     //作者
     private static String authorName = "whj";
     //要生成的表名，逗号隔开
-//    private static String[] tables = {"t_ty_category", "t_ty_constructor", "t_ty_device", "t_ty_maintain_order", "t_ty_order", "t_ty_project", "t_ty_room"};
-    private static String tableList = "t_ty_category,t_ty_constructor,t_ty_device,t_ty_maintain_order,t_ty_order,t_ty_project,t_ty_room";
+    private static String tableList = "sh_device_key";
     //表名前缀，用于生成的类名去掉前缀
+    private static final String tablePrefix = "sh_";
     //数据库配置四要素
     private static DbType dbType = DbType.MYSQL;
     private static String driverName = "com.mysql.cj.jdbc.Driver";
-    private static String url = "jdbc:mysql://192.168.0.108:3306/ty_platform?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
+    private static String url = "jdbc:mysql://192.168.0.108:3306/smart-home?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
     private static String username = "root";
     private static String password = "123456";
 
-    private final static Logger logger = LoggerFactory.getLogger(CodeGenerator.class);
 
     /**
      * 获取项目路径
@@ -42,7 +38,7 @@ public class CodeGenerator {
     static {
         try {
             canonicalPath = new File("").getCanonicalPath();
-            logger.info("项目绝对路径:{}", canonicalPath);
+            System.out.println("项目绝对路径:" + canonicalPath);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -73,10 +69,14 @@ public class CodeGenerator {
         mpg.setDataSource(dsc);
 
         // 包配置
-        PackageConfig pc = new PackageConfig().setParent(basePackage);
+        PackageConfig pc = new PackageConfig()
+                .setParent(basePackage)
+                .setXml("xml");
         mpg.setPackageInfo(pc);
-        String[] tables = tableList.split(",");
+
+
         // 策略配置
+        String[] tables = tableList.split(",");
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
@@ -84,7 +84,7 @@ public class CodeGenerator {
         strategy.setRestControllerStyle(true);
         strategy.setControllerMappingHyphenStyle(false);
         strategy.setInclude(tables);
-        strategy.setTablePrefix("t_");
+        strategy.setTablePrefix(tablePrefix);
         mpg.setStrategy(strategy);
         mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         mpg.execute();
